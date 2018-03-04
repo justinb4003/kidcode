e148970c903f32441ec779a02689bc7b13303bac
Justin Buist <justin.buist@gmail.com>
2018-03-04 10:18:07 -0500

    Added sound code.

--- namespell.py
+++ namespell.py
@@ -2,21 +2,60 @@
 # Copyright Justin Buist
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+import pango
 import pygtk
 import gtk
+import gobject
+import pyaudio
+import wave
+import threading
 pygtk.require('2.0')
+
+TARGET_TEXT = 'HARRIET'
 
 # GTK Ops
 def delete_event(widget, event, data=None):
-    print('Delete hit.')
     return False
 
 def destroy(widget, data=None):
-    print('Destroy hit.')
     gtk.main_quit()
-    app_running = False
-    print('main_quit() completed.')
 
+def playSound(filename):
+    chunk = 1024
+    f = wave.open(filename, "rb")
+    p = pyaudio.PyAudio()
+    stream = p.open(format = p.get_format_from_width(f.getsampwidth()),
+                    channels = f.getnchannels(),
+                    rate = f.getframerate(),
+                    output = True)
+    data = f.readframes(chunk)
+    while data:
+        stream.write(data)
+        data = f.readframes(chunk)
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+def playFinish():
+    playSound('applause.wav')
+
+def playReset():
+    playSound('boing.wav')
+
+def name_onKeyPress(widget):
+    inputText = widget.get_text()
+    subTarget = TARGET_TEXT[:len(inputText)]
+    if (inputText.lower() == subTarget.lower()):
+        if (len(inputText) == len(TARGET_TEXT)):
+            widget.set_text('')
+            t = threading.Thread(target=playFinish)
+            t.start() 
+    else:
+        playReset()
+        widget.set_text('')
+        
+
+gobject.threads_init()
 
 window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 window.set_border_width(10)
@@ -25,15 +64,16 @@ window.connect("destroy", destroy)
 
 vbox = gtk.VBox()
 
-nameLabel = gtk.Label()
-nameLabel.set_markup('<span font_size="xx-large"><tt>HARRIET</tt></span>')
-nameLabel.set_use_markup(True)
+nameFont = pango.FontDescription("Monospace 24")
 
-nameInput = gtk.TextView()
-nameInputBuf = nameInput.get_buffer()
-sz = nameInputBuf.create_tag(size_points=40, weight=10, justification="left")
-pos = nameInputBuf.get_end_iter()
-nameInputBuf.insert_with_tags(pos, 'HI', sz)
+nameLabel = gtk.Label()
+nameLabel.set_justify(gtk.JUSTIFY_LEFT)
+nameLabel.modify_font(nameFont)
+nameLabel.set_text(TARGET_TEXT)
+
+nameInput = gtk.Entry()
+nameInput.modify_font(nameFont)
+nameInput.connect('changed', name_onKeyPress)
 
 vbox.add(nameLabel)
 vbox.add(nameInput)
